74482c37fe76f5562e48183ba7389f3613d2c874
zaidanammar <zaidanammar23@gmail.com>
2021-11-04 16:15:02 +0700

    :zap: add env variable

--- config/AxiosInstance.ts
+++ config/AxiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = () => {
   return axios.create({
-    baseURL: 'https://jajanan-kedai-atok-server.herokuapp.com/',
+    baseURL: process.env.NEXT_PUBLIC_API,
   });
 };
 
