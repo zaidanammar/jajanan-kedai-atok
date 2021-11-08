8d39c8aa71d085fe6e40bfdb0962f2cf0367cadc
zaidanammar <zaidanammar23@gmail.com>
2021-11-08 17:35:04 +0700

    :zap: add aos animation v.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import { useState } from "react";
-import Aos from 'aos';
+import { useEffect, useState } from "react";
+import Aos from "aos";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  Aos?.init()
+
+  useEffect(() => {
+    Aos?.init();
+  }, []);
+  
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
