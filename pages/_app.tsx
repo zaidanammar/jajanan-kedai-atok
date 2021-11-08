d25f2b751301f95638f6bb12b2e7d26037841e28
zaidanammar <zaidanammar23@gmail.com>
2021-11-08 17:28:56 +0700

    :zap: add aos animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { useState } from "react";
+import Aos from 'aos';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  Aos?.init()
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import Layout, { HeaderProps } from "../components/layout/layout";
 import { bannerImg, logo } from "../assets/index";
 import { getBanners } from "../apis/banner/banner";
-
+import "aos/dist/aos.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay, Parallax, Pagination, Navigation } from "swiper";
 
@@ -29,11 +29,48 @@ const Home: NextPage = () => {
   const { data, isLoading } = useQuery("banners", getBanners);
   return (
     <Layout header={header}>
-      <main className="mt-96 xl:pt-72 lg:pt-64 md:pt-52 sm:pt-40 pt-36 overflow-x-hidden">
-        <h1 className=" text-pink-600">TESSSS</h1>
-        <h1 className={styles.title}>
-          Welcome to <Link href="/menu">Menu</Link>
-        </h1>
+      <main className="mt-96 xl:pt-72 lg:pt-64 md:pt-52 sm:pt-40 pt-36 overflow-x-hidden flex flex-col items-center">
+        <div
+          className="flex justify-center"
+          data-aos="zoom-out-down"
+          data-aos-duration="1000"
+        >
+          <h1 className="text-2xl">Mengapa Jajanan Kedai Atok ?</h1>
+        </div>
+        <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-14  my-10 bg-gray-100 ">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            className="bg-gray-400 w-96 h-40"
+          >
+            <h1>JAJANAN KEDAI ATOK</h1>
+          </div>
+        
+          <div
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            className="bg-gray-400 w-96 h-40"
+          >
+            <h1>JAJANAN KEDAI ATOK</h1>
+          </div>
+        
+          <div
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            className="bg-gray-400 w-96 h-40"
+          >
+            <h1>JAJANAN KEDAI ATOK</h1>
+          </div>
+        
+          <div
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            className="bg-gray-400 w-96 h-40"
+          >
+            <h1>JAJANAN KEDAI ATOK</h1>
+          </div>
+        
+        </section>
       </main>
     </Layout>
   );
